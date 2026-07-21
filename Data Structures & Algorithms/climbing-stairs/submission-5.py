a3b4541db8db369aa034bf6e65d494d1f5856b20
FukamiRui <fukami.rui@icloud.com>
2026-07-21 17:05:13 +0900

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev1 = 2
+        prev2 = 1
+
+        if n <= 2:
+            return n
+
+        for i in range(3, n+1):
+            curr = prev1 + prev2
+            prev1, prev2 = curr, prev1
+        return prev1
+        
