3c9238970cb2cc88e87a4360e3df3fd978e02695
FukamiRui <fukami.rui@icloud.com>
2026-07-21 19:04:00 +0900

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closures = {")": "(", "]": "[", "}": "{" }
+        stack = []
+
+        for i in s:
+            if i in closures:
+                if stack and stack[-1] == closures[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
+
+
+        
